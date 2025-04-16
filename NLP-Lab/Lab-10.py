971da5584a0d13f3baa065deced606e7761f7deb
Aakarsh Pathak <113720231+Aakarsh204@users.noreply.github.com>
2025-04-16 12:06:14 +0530

    Update Lab-10.py

--- NLP-Lab/Lab-10.py
+++ NLP-Lab/Lab-10.py
@@ -4,13 +4,13 @@ from nltk.tag import hmm
 
 tagged_sents = brown.tagged_sents(tagset = 'universal')
 
-train_data = tagged_sents[:5000]
-test_data = tagged_sents[5000:5050]
+train_data = tagged_sents[:50000]
+test_data = tagged_sents[50000:50500]
 
 trainer = hmm.HiddenMarkovModelTrainer()
 hmm_model = trainer.train_supervised(train_data)
 
-accuracy = hmm_model.evaluate(test_data)
+accuracy = hmm_model.accuracy(test_data)
 print(f'Accuracy: {accuracy:.2f}')
 
 sentence = "The quick brown fox jumped over the lazy dog".split()
